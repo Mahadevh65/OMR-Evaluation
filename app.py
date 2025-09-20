f29d520ba72baaf95542f51ecb9a3d0219f82db5
Mahadev <mahadevhulsure65@gmail.com>
2025-09-20 17:18:21 +0530

    changes in requirement.txt file

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def upload_file():
         return render_template("upload.html", versions=versions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
